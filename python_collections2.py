254c2230018c5ddadae084d2b33b9b484610ac7e
gualourenco <gualourenco@gmail.com>
2022-03-04 13:34:08 -0300

    atualizando estudos

--- python_collections2.py
+++ python_collections2.py
@@ -1,5 +1,6 @@
 from collections import defaultdict, Counter
 
+
 class Conta:
 
     def __init__(self):
@@ -133,3 +134,63 @@ print(contas[15])
 
 aparicoes3 = Counter(meu_texto)
 print(aparicoes3)
+
+texto1 = '''Seu trabalho de stand-up tem sido um fio condutor de sua carreira, como pode se ver pelo sucesso de seu show
+ (e o DVD subsequente) Robin Williams: Live on Broadway (2002). Obteve em 2004 o 13º lugar na lista de "100 maiores 
+ [comediantes] stand-up de todos os tempos" do canal Comedy Central.
+Após ser encorajado por sua amiga, a também comediante Whoopi Goldberg, decidiu aceitar uma participação especial na 
+série Star Trek: The Next Generation, em 1991, no episódio "A Matter of Time", porém teve que cancelar sua aparição 
+devido a um conflito de calendário; Matt Frewer assumiu seu lugar no papel do professor Berlingoff Rasmussen,
+um viajante do tempo trapaceiro.
+Williams também apareceu, em 2000, num episódio da versão americana de Whose Line Is It Anyway?.
+Durante um jogo chamado "Scenes from a Hat" ("cenas de um chapéu"), a cena "What Robin Williams is thinking right now"
+("O que Robin Williams está pensando agora") foi criada, e Williams declarou: "I have a career.
+What the hell am I doing here?" ("Eu tenho uma carreira. Que diabos estou fazendo aqui agora?")
+'''
+
+texto2 = '''De Niro é descendente de italianos,[2][3] sendo também descendente de irlandeses, alemães e holandeses.
+[carece de fontes] Apesar disso, o ator diz que se identifica "mais com o seu lado italiano que com os outros lados".
+Em outubro de 2004 cancelou uma apresentação em Roma, após as autoridades italianas o terem acusado
+de apresentar estereótipos negativos da sua ascendência nos seus filmes.
+Em dezembro do mesmo ano voltou a Roma para apresentar pela primeira vez uma exposição de arte do seu falecido pai.
+As mesmas autoridades, surpreendidas, disseram que não guardavam ressentimentos.[carece de fontes]
+de Niro em 2008
+O ator tem verdadeira paixão por Nova Iorque, particularmente pelo bairro que habita, TriBeCa, na baixa Manhattan.
+Desde 1989, investe na região, tendo aberto lá um restaurante, uma produtora, e, principalmente, criado o festival 
+de cinema independente, TriBeCa Film Festival, festival que vem ganhando importância a cada ano que passa.
+'''
+
+
+# print(Counter(texto1.lower()))
+# aparicoes = Counter(texto1.lower())
+#
+# print(aparicoes.values())
+# total_de_caracteres = sum(aparicoes.values())
+
+# for letra, frequencia in aparicoes.items():
+#     tupla = (letra, frequencia / total_de_caracteres)
+#     print(tupla)
+
+
+# proporcoes = [(letra, frequencia / total_de_caracteres) for letra, frequencia in aparicoes.items()]
+# proporcoes = dict(proporcoes)
+#
+# proporcoes = Counter(dict(proporcoes))
+# proporcoes.most_common(10)
+
+
+def analisa_frequencia_de_letras(texto):
+
+    aparece = Counter(texto.lower())
+    total_de_caracteres = sum(aparece.values())
+
+    proporcoes = [(letra, frequencia / total_de_caracteres) for letra, frequencia in aparece.items()]
+    proporcoes = Counter(dict(proporcoes))
+    mais_comuns = proporcoes.most_common(10)
+
+    for caractere, proporcao in mais_comuns:
+        print('{} => {:.2f}%'.format(caractere, proporcao * 100))
+
+
+print(analisa_frequencia_de_letras(texto1))
+print(analisa_frequencia_de_letras(texto2))
